77b1e2ee61bfaa5346d1b0f724d40802899ec2bc
Pad7 <Myself@mypad.com>
2025-03-24 19:25:12 +0800

    Finish the design and test of a Vector class.

--- 1Cplusplus/2 Vector.cpp
+++ 1Cplusplus/2 Vector.cpp
@@ -1,62 +1,197 @@
 #include <iostream>
 
+using namespace std;
 struct Node
 {
+public:
 	double Data;
 	Node* Next;
-	Node(double input, Node* link = nullptr) : Data(input), Next(link) {}
+	Node(double input1, Node* input2 = nullptr) : Data(input1), Next(input2) {}
 };
+
 class Vector
 {
-	private:
-		Node* Head;
-		Node* Tail;
-		int Length;
-	public:
-		Vector():Length(0)
+protected:
+	Node* Head;
+	Node* Tail;
+	int len;
+	Vector& Enqueue(double input)
+	{
+		Tail = Tail->Next = new Node(input,nullptr);
+		len++;
+		return *this;
+	}
+
+public:
+	Vector(double*input = nullptr, int size = 0):len(0)
+	{
+		Head = Tail = new Node(0,nullptr);
+		while(len < size)
 		{
-			Head = Tail = new Node(0);
+			Tail = Tail->Next = new Node(input[len],nullptr);
+			++len;
 		}
-		~Vector()
+	}
+
+	Vector(const Vector& another):len(0)
+	{
+		Head = Tail = new Node(0,nullptr);
+		const Node* read = another.Head->Next;
+		while(read != nullptr)
 		{
-			Node* temp = Head->Next;
-			while(temp != nullptr)
+			this->Enqueue(read->Data);
+			read = read->Next;
+		}
+	}
+
+	~Vector()
+	{
+		Node* temp = Head->Next;
+		while(temp != nullptr)
+		{
+			Head->Next = Head->Next->Next;
+			delete[] temp;
+			temp = Head->Next;
+		}
+		delete Head;
+	}
+
+	double& operator[](int index)
+	{
+		if (len == 0 || index > len)
+		{
+			cout << "Index out of range.\n";
+			return Head->Data;
+		}
+		
+		int count = 0;
+		Node* read = Head->Next;
+		while(count < index && read != nullptr)
+		{	
+			++count;
+			read = read->Next;
+		}
+		return read->Data;
+	}
+
+	friend istream& operator>>(istream& input, Vector& operated)
+	{
+		cout << "Enter elements of a vector and end with ')': \n(";
+		double temp;
+		int control;
+		while((control = input.peek()) != ')')
+		{
+			if (control > '9' || control < '0')
 			{
-				Head->Next = Head->Next->Next;
-				delete temp;
-				temp = Head->Next;
+				input.get();
+				continue;
 			}
-			delete Head;
+			input >> temp;
+			operated.Enqueue(temp);
+		}
+		input.get();
+		return input;
+	}
+
+	friend ostream& operator<<(ostream& output, const Vector& operated)
+	{
+		output << "Vector: \t(";
+		Node* read = operated.Head->Next;
+		while(read != nullptr)
+		{
+			output << read->Data;
+			if (read->Next != nullptr)
+				output << ", ";
+			read = read->Next;
+		}
+		output << ")\n";
+		return output;
+	}
+
+	Vector& operator=(const Vector& source)
+	{
+		// clear this
+		Node* read = this->Head->Next;
+		while(read != nullptr)
+		{
+			this->Head->Next = this->Head->Next->Next;
+			delete[] read;
+			read = this->Head->Next;
+		}
+		this->Tail = this->Head;
+		len = 0;		
+		// copy source to this
+		read = source.Head->Next;
+		while(read != nullptr)
+		{
+			this->Enqueue(read->Data);
+			read = read->Next;
 		}
 
-		Vector(const Vector& another)
-		{
-			 Head = Tail = new Node(0);
-			 const Node* read = another.Head->Next;
-			 while(read != nullptr)
-			 {
-				Tail = Tail->Next = new Node(read->Data);
-				read = read->Next;
-			 }
-		}
+		return *this;
+	}
 
-		Vector& operator= (const Vector& another)
+	friend Vector operator+(const Vector& a, const Vector& b)
+	{
+		static Vector error;
+		if (a.len != b.len) return error;
+		Vector temp;
+		const Node* read1 = a.Head->Next;
+		const Node* read2 = b.Head->Next;
+		while(read1 != nullptr)
 		{
-			Node* temp = Head->Next;
-			while(temp != nullptr)
-			{
-				Head->Next = Head->Next->Next;
-				delete temp;
-				temp = Head->Next;
-			}
-			delete Head;
-			this->Head = this->Tail = new Node(0);
-
-			const Node* read = another.Head->Next;
-			while(read != nullptr)
-			{
-				Tail = Tail->Next = new Node(read->Data);
-				read = read->Next;
-			}
+			temp.Enqueue(read1->Data + read2->Data);
+			read1 = read1->Next;
+			read2 = read2->Next;
 		}
+		cout << temp;
+		return Vector(temp);
+	}
+	friend Vector operator-(const Vector& a, const Vector& b)
+	{
+		static Vector error;
+		if (a.len != b.len) return error;
+		Vector temp;
+		const Node* read1 = a.Head->Next;
+		const Node* read2 = b.Head->Next;
+		while(read1 != nullptr)
+		{
+			temp.Enqueue(read1->Data - read2->Data);
+			read1 = read1->Next;
+			read2 = read2->Next;
+		}
+		return temp;
+	}
+	friend double operator*(const Vector& a, const Vector& b)
+	{
+		if (a.len != b.len) return 0.0;
+		const Node* read1 = a.Head->Next;
+		const Node* read2 = b.Head->Next;
+		double result = 0.0;
+		while (read1 != nullptr)
+		{
+			result += read1->Data * read2->Data;
+			read1 = read1->Next;
+			read2 = read2->Next;
+		}
+		return result;
+	}
 };
+
+int main()
+{
+	Vector A;
+	Vector B;
+	std::cout << "Hello world!\n";
+	cin >> A >> B;
+	cout << A << B;
+
+	Vector C = A + B;
+	cout << "Sum of inputed vectors: " << C;
+	C = A - B;
+	cout << "Difference of inputed vectors: " << C;
+	cout << "The 3rd element of the vector below is: " << C[2] << endl;
+
+	cout << "Product of inputed vectors: " << A*B << endl;
+	return 0;
+}
